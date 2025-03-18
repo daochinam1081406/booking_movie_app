de2911ad63956463a4ff2c08b970cfe342bd9de0
daochinam1081406 <46882575+daochinam1081406@users.noreply.github.com>
2025-03-19 01:28:40 +0700

    Update config.js

--- src/apis/config.js
+++ src/apis/config.js
@@ -10,7 +10,6 @@ export const BASE_URL = "https://movienew.cybersoft.edu.vn/api/";
 
 export const TOKEN_CYBER =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3NSIsIkhldEhhblN0cmluZyI6IjE0LzA1LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0NzE4MDgwMDAwMCIsIm5iZiI6MTcyMDg5MDAwMCwiZXhwIjoxNzQ3MzI4NDAwfQ.bqygxoVHbmcy6bdDT5IDHZGoA3eMAp4YV6_E_dO_XxI";
     
- const 
 export const configHeaders = () => {
   return { TokenCybersoft: TOKEN_CYBER };
 };
